bcc5869bf618d2ba85e16f4b975f72d56cb3f2ae
fayesadev <fayedumbrigue@gmail.com>
2023-01-10 04:36:13 +0000

    Using console.assert to compare if two entries are equivalent or not

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, actual + " !== " + expected + " ⛔️⛔️⛔️");
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("Hello", "hello");
+assertEqual(40, 40);
+assertEqual(2, '2');
+assertEqual(10, 'ten');
