0bb016c8bb89c464ebe4c47947bc4039196c06e1
fayesadev <fayedumbrigue@gmail.com>
2023-01-12 03:31:17 +0000

    Function that returns key of given object when given a value

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${actual} !== ${expected} ⛔️⛔️⛔️`);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+}
+
+const findKeyByValue = function(obj, value) {
+  for (let property in obj) {
+    if (obj[property] === value) {
+    return property;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const bestFoodByCuisine = { 
+  Japanese: "Ramen",
+  Indian: "Butter Chicken",
+  Korean:  "Cheese Buldak"
+};
+
+assertEqual(findKeyByValue(bestFoodByCuisine, "Ramen"), "Japanese");
+assertEqual(findKeyByValue(bestFoodByCuisine, "Cheese Buldak"), "Korean");
