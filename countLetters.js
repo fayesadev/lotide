dcb54e11d1cd620f2f93b7217fc88c9235e54582
fayesadev <fayedumbrigue@gmail.com>
2023-01-12 01:18:11 +0000

    function that counts each letter in a given string

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${actual} !== ${expected} ⛔️⛔️⛔️`);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+}
+
+const countLetters = function(sentence) {
+  let letterCount = {};
+  sentence = sentence.replace(/ /g, '');
+
+  for (let letter of sentence) {
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+}
+
+console.log(countLetters('lighthouse in the house'));
