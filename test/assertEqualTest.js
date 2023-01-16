bc6d5b2e48e7872a8907494dfa20f8bc5e594f21
fayesadev <fayedumbrigue@gmail.com>
2023-01-16 22:37:46 +0000

    Created separate test folder for tests, implementing exports and requires

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("Hello", "hello");
+assertEqual(40, 40);
+assertEqual(2, '2');
+assertEqual(10, 'ten');
