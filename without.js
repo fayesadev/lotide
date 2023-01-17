66c0809b5fea7ea0c1ccb489f26fafa3895418d3
fayesadev <fayedumbrigue@gmail.com>
2023-01-17 04:51:37 +0000

    modified tests for assertArraysEqual

--- without.js
+++ without.js
@@ -12,9 +12,9 @@ const without = function(source, itemsToRemove) {
 
 module.exports =  without;
 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]) // => ["1", "2"]
 const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
