9a8550c9fceba70b6ed509ec2118e6444ac0dbfa
fayesadev <fayedumbrigue@gmail.com>
2023-01-17 01:05:56 +0000

    Implemented mocha and chai test assertions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,23 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-//returns false because assertEqual cannot compare arrays
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+describe('#tail', () => {
+  it("returns the array with the first element removed", () => { 
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
 
-//assertEqual can confirm if the function is not modifying the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it("checks if the the function is not modifying the original array", () => { 
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
 
-//case test: an array with only one element yields an empty array for tail()
-assertEqual(tail([1]).length, 0);
+  it("returns [] with an array with only one element", () => { 
+    assert.deepEqual(tail(["Hello"]), []);
+  });
 
-//case test: an empty array should yield an empty array for tail()
-assertEqual(tail([]).length, 0);
+  it("returns [] with an empty array", () => { 
+    assert.deepEqual(tail([]), []);
+  });
+
+});
